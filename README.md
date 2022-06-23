eb2218b80f7761cceacbfca7815adb539a660f37
Taiwo Oluwagbemiga <49463561+Newkoncept@users.noreply.github.com>
2022-06-23 12:53:47 +0100

    New files

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# zuri_js_intro
+# Zuri Javascript Introduction
+
+![alt-text](screen.png)
+
+### [Link to the published page](htps:wwww.google.com)
---
+++ app.js
@@ -0,0 +1,10 @@
+const div = document.createElement("div");
+
+div.innerHTML = `
+    <h1>My Details</h1>
+    <p><strong>Name:</strong> Oluwagbemiga Taiwo</p>
+    <p><strong>Height:</strong> 176.5 cm </p>
+    <p><strong>Country:</strong> Nigeria</p>
+`;
+
+document.body.append(div);
